373b1a57c1156352caec741b25eaba5801b7b573
justgotintocoding <64993117+justgotintocoding@users.noreply.github.com>
2020-09-22 11:04:23 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,10 @@
+input.onButtonPressed(Button.A, function () {
+basic.showIcon(IconNames.Happy)    
+})
+input.onButtonPressed(Button.B, function () {
+ basic.showIcon(IconNames.Sad)   
+})
+input.onButtonPressed(Button.AB, function () {
+ basic.showIcon(IconNames.Silly)   
+basic.showIcon(IconNames.Surprised)
+})
